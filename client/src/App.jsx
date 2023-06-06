ef5451bef9b39bc10754922b7517faba4e8b6b69
4rnav <arnavsarkar10@gmail.com>
2023-06-06 12:04:12 +0530

    styling changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [file, setFile] = useState('');
+
+
+  const fileInputRef = useRef();
+  const onUploadClick = () => {
+    //link file input to upload button using fileInputRef
+    fileInputRef.current.click();
+  }
+
+
+
   return (
     <>
-      <div>Hello</div>
+      <div className='container'>
+
+        <div className='wrapper'>
+          <h1>FileShare.io</h1>
+          <p>Upload and share the download link.</p>
+
+          <button className="button-27" onClick={() => onUploadClick()}>Upload</button>
+
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+
+
+        </div>
+      </div>
     </>
   )
 }
